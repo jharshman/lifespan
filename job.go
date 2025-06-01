2c2f4e63e8985f26056df5cfbe6b17c50ad11894
jharshman <josh@harshman.me>
2025-06-01 14:13:45 -0600

    update package with new pattern

--- job.go
+++ job.go
@@ -1,107 +1,52 @@
-/*
-Package lifespan helps to facilitate the safe running and closing of Jobs.
-
-To get started, let's take a look at lifespan.Job. As you can see in the example below
-we are defining a lifespan.Job called myJob, and stubbing out the Run and Close fields.
-These fields are functions that will control the running and safe closing of your Job.
-
-	myJob := lifespan.Job{
-		Run: func(ctx context.Context) error {
-			// do my thing
-		},
-		Close: func(ctx context.Context) error {
-			// close my thing
-		},
-	}
-
-Running an HTTP server with lifespan.Job might look like the following:
-
-	myJob := lifespan.Job{
-		Run: func(ctx context.Context) error {
-			return http.ListenAndServe()
-		},
-		Close: func(ctx.Context.Context) error {
-			return s.Shutdown(context.Background())
-		},
-	}
-
-	myJob.RunAndClose()
-
-The provided lifespan.Job implements the SafeCloser interface.
-You can use this to make your own implementation suit your needs.
-*/
 package lifespan
 
 import (
 	"context"
-	"os"
-	"os/signal"
-	"syscall"
+	"log/slog"
+	"time"
 )
 
-// SafeCloser defines the behavior expected for a Job or other SafeCloser implementation.
-// It is expected that RunWithClose not only runs a thing, but also knows how to safely exit it.
-type SafeCloser interface {
-	RunWithClose(ctx context.Context) (err chan error)
+type Runnable interface {
+  Run(span *LifeSpan)
 }
 
-// Job is a simple implementation of SafeCloser. The Run and Close methods define what a task should do and how it
-// should end. Job also contains an array of os.Signal. These are used by RunWithClose to determine when to trigger the Close()
-// function.
-type Job struct {
-	// Run And Close functions.
-	Run   func() error
-	Close func() error
-	// Signals is a slice of os.Signal to notify on.
-	Signals []os.Signal
+type LifeSpan struct {
+  Sig, Ack chan struct {}
+  Err chan error
+  Ctx context.Context
+  Cancel context.CancelFunc
 }
 
-// RunWithClose is the implementation of SafeCloser for Job. In this method, the Job.Run function is called within a
-// goroutine. When a signal is received indicating it is time for Job.Run to end, the Job.Close function is invoked.
-// An acknowledgement indicating the Job.Close method is finished is then sent to the control loop.
-// This method is non-blocking. If any waiting or blocking need occur, it must happen outside of this implementation.
-func (j *Job) RunWithClose(ctx context.Context) (err chan error) {
-	sig := make(chan int, 1)
-	ack := make(chan int, 1)
-	err = make(chan error, 1)
-
-	closeChan := make(chan os.Signal, 1)
-	if len(j.Signals) == 0 {
-		j.Signals = []os.Signal{
-			syscall.SIGINT,
-			syscall.SIGTERM,
-		}
-	}
-	signal.Notify(closeChan, j.Signals...)
-
-	// Run and Close
-	go func() {
-		go func() {
-			if e := j.Run(); e != nil {
-				err <- e
-			}
-		}()
-		<-sig
-		if e := j.Close(); e != nil {
-			err <- e
-		}
-		ack <- 1
-	}()
-
-	// Control
-	go func() {
-	LOOP:
-		for {
-			select {
-			case <-closeChan:
-				sig <- 1
-			case <-ack:
-				break LOOP
-			case <-ctx.Done():
-				break LOOP
-			}
-		}
-	}()
-
-	return
+func (span *LifeSpan) Close() {
+  select {
+  case span.Sig <- struct{}{}:
+    select {
+    case <-span.Ack:
+      return
+    case <-time.After(3 * time.Second):
+      slog.Warn("timeout waiting for ack")
+    }
+  default:
+  }
 }
+
+func Run(f func(span *LifeSpan)) (span *LifeSpan) {
+  ctx, cancel := context.WithCancel(context.Background())
+
+  span = &LifeSpan{
+    Sig: make(chan struct{}, 1),
+    Ack: make(chan struct{}, 1),
+    Err: make(chan error, 1),
+    Ctx: ctx,
+    Cancel: cancel,
+  }
+
+  go func() {
+    defer close(span.Ack)
+    defer cancel()
+    f(span)
+  }()
+
+  return
+}
+
--- job_test.go
+++ job_test.go
@@ -1,194 +1,32 @@
 package lifespan_test
 
 import (
-	"context"
-	"errors"
-	"net/http"
-	"os"
-	"os/signal"
-	"syscall"
+  "fmt"
 	"testing"
-	"time"
+  "time"
 
 	"github.com/jharshman/lifespan"
 )
 
-func Test_RunWithClose(t *testing.T) {
-	s := http.Server{
-		Addr:    ":8080",
-		Handler: http.DefaultServeMux,
-	}
+func Test_Run(t *testing.T) {
+  span := lifespan.Run(func(span *lifespan.LifeSpan) {
+    LOOP: for {
+      select {
+      case <-span.Ctx.Done():
+        break LOOP
+      case <-span.Sig:
+        break LOOP
+      default:
+      }
+      fmt.Println("hello from Run function")
+      time.Sleep(1 * time.Second)
+    }
+    span.Ack <- struct{}{}
+  })
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.WriteHeader(http.StatusOK)
-		w.Write([]byte("OK"))
-	})
+  time.Sleep(5 * time.Second)
+  fmt.Println("exiting")
+  span.Close()
 
-	job := lifespan.Job{
-		Run: func() error {
-			return s.ListenAndServe()
-		},
-		Close: func() error {
-			return s.Shutdown(context.Background())
-		},
-	}
-
-	sig, ack, err := job.RunWithClose()
-
-	closeChan := make(chan os.Signal, 1)
-	signal.Notify(closeChan, syscall.SIGTERM, syscall.SIGINT)
-
-	// go routine to notify close after short wait
-	go func() {
-		<-time.After(time.Second * 5)
-		syscall.Kill(syscall.Getpid(), syscall.SIGINT)
-	}()
-
-LOOP:
-	for {
-		select {
-		case <-closeChan:
-			sig <- 1
-			break LOOP
-		case <-ack:
-			break LOOP
-		case e := <-err:
-			t.Errorf("%v\n", e)
-			break LOOP
-		}
-	}
 }
 
-func TestJob_Execute(t *testing.T) {
-	s := http.Server{
-		Addr:    ":8080",
-		Handler: http.DefaultServeMux,
-	}
-
-	http.HandleFunc("/foo", func(w http.ResponseWriter, r *http.Request) {
-		w.WriteHeader(http.StatusOK)
-		w.Write([]byte("OK"))
-	})
-
-	job := lifespan.Job{
-		Run: func() error {
-			return s.ListenAndServe()
-		},
-		Close: func() error {
-			return s.Shutdown(context.Background())
-		},
-		Signals: []os.Signal{syscall.SIGINT},
-	}
-
-	// go routine to notify close after short wait
-	go func() {
-		<-time.After(time.Second * 5)
-		syscall.Kill(syscall.Getpid(), syscall.SIGINT)
-	}()
-
-	err := job.Execute()
-	if err != nil {
-		t.Error(err)
-	}
-}
-
-func TestJob_ExecuteRunWithErrors(t *testing.T) {
-	job := lifespan.Job{
-		Run: func() error {
-			return errors.New("some error")
-		},
-		Close: func() error {
-			return nil
-		},
-		Signals: []os.Signal{syscall.SIGINT},
-	}
-
-	// go routine to notify close after short wait
-	go func() {
-		<-time.After(time.Second * 5)
-		syscall.Kill(syscall.Getpid(), syscall.SIGINT)
-	}()
-
-	// error expected here
-	err := job.Execute()
-	if err == nil {
-		t.Error(err)
-	}
-}
-
-func TestJob_ExecuteCloseWithErrors(t *testing.T) {
-	job := lifespan.Job{
-		Run: func() error {
-			return nil
-		},
-		Close: func() error {
-			return errors.New("some error")
-		},
-		Signals: []os.Signal{syscall.SIGINT},
-	}
-
-	// go routine to notify close after short wait
-	go func() {
-		<-time.After(time.Second * 5)
-		syscall.Kill(syscall.Getpid(), syscall.SIGINT)
-	}()
-
-	// error expected here
-	err := job.Execute()
-	if err == nil {
-		t.Error(err)
-	}
-}
-
-func TestJob_ExecuteNoCloseDefined(t *testing.T) {
-	job := lifespan.Job{
-		Run: func() error {
-			return nil
-		},
-		Signals: []os.Signal{syscall.SIGINT},
-	}
-
-	// error expected here
-	err := job.Execute()
-	if err == nil {
-		t.Error(err)
-	}
-}
-
-func TestJob_ExecuteNoRunDefined(t *testing.T) {
-	job := lifespan.Job{
-		Close: func() error {
-			return nil
-		},
-		Signals: []os.Signal{syscall.SIGINT},
-	}
-
-	// error expected here
-	err := job.Execute()
-	if err == nil {
-		t.Error(err)
-	}
-}
-
-func TestJob_SignalToClose(t *testing.T) {
-	job := lifespan.Job{
-		Run: func() error {
-			return nil
-		},
-		Close: func() error {
-			return nil
-		},
-		Signals: []os.Signal{syscall.SIGINT},
-	}
-
-	// go routine to notify close after short wait
-	go func() {
-		<-time.After(time.Second * 5)
-		job.SignalToClose()
-	}()
-
-	err := job.Execute()
-	if err != nil {
-		t.Error(err)
-	}
-}
